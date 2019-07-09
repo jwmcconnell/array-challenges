a5fdf531bc32c8958661ac1a829af44f6a08cf2e
Jackmc <jackmc1122@gmail.com>
2019-07-08 17:16:48 -0700

    Handle the negative case.

--- test/findMissingNumber.test.js
+++ test/findMissingNumber.test.js
@@ -10,4 +10,9 @@ describe('findMissingNumber', () => {
     const result = findMissingNumber([2, 5, 1, 4, 9, 6, 3, 7], 9, 1);
     expect(result).toEqual(8);
   });
+
+  it('returns the missing number in an array', () => {
+    const result = findMissingNumber([2, -1, -2, 5, 1, 4, 9, 8, 6, 3, 7], 9, -3);
+    expect(result).toEqual(-3);
+  });
 });

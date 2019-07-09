ac1a9b5b63351b8ca50a4d41c3e60eed0644bde9
Jackmc <jackmc1122@gmail.com>
2019-07-08 17:03:40 -0700

    Find missing number using n^2 solution.

--- test/findMissingNumber.test.js
+++ test/findMissingNumber.test.js
@@ -2,7 +2,12 @@ const { findMissingNumber } = require('../lib/findMissingNumber');
 
 describe('findMissingNumber', () => {
   it('returns a number', () => {
-    const result = findMissingNumber([1, 2, 3, 4], 1, 4);
+    const result = findMissingNumber([2, 5, 1, 4, 9, 6, 3, 7], 9, 1);
     expect(result).toEqual(expect.any(Number));
+  });
+
+  it('returns the missing number in an array', () => {
+    const result = findMissingNumber([2, 5, 1, 4, 9, 6, 3, 7], 9, 1);
+    expect(result).toEqual(8);
   });
 });

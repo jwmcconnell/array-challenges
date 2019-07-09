d83287e4821c7549222c5257d10a12c1565bb4ee
Jackmc <jackmc1122@gmail.com>
2019-07-08 17:40:43 -0700

    Use a set to get all unique values from the array.

--- test/uniqueValues.test.js
+++ test/uniqueValues.test.js
@@ -5,4 +5,9 @@ describe('uniqueValues', () => {
     const result = uniqueValues([1, 2, 3, 4, 5]);
     expect(result).toEqual(expect.any(Array));
   });
+
+  it('returns an array with only the unique values from the original', () => {
+    const result = uniqueValues([3, 3, 5, 3, 12, 7]);
+    expect(result).toEqual([3, 5, 12, 7]);
+  });
 });

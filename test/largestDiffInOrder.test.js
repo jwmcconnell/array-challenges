fb698874dc76bae3ae331078e9560ebb1c1e3eb3
Jackmc <jackmc1122@gmail.com>
2019-07-08 19:40:03 -0700

    Brute force largest diff in order.

--- test/largestDiffInOrder.test.js
+++ test/largestDiffInOrder.test.js
@@ -5,4 +5,9 @@ describe('largestDiffInOrder', () => {
     const result = largestDiffInOrder([7, 8, 4, 9, 9, 15, 3, 1, 10]);
     expect(result).toEqual(expect.any(Number));
   });
+
+  it('returns the largest difference between two numbers in the array according to order', () => {
+    const result = largestDiffInOrder([7, 8, 4, 9, 9, 15, 3, 1, 10]);
+    expect(result).toEqual(11);
+  });
 });
